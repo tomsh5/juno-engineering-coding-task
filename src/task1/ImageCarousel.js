33ca34ac369f2522cd5f864951816c805fbce827
tomsh5 <tomsh5@gmail.com>
2022-09-05 16:00:02 +0300

    adding empty state

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -12,34 +12,44 @@ const ImageCarousel = () => {
   const [imgUrls, setImgUrls] = useState([]);
   const [currImgIndex, setCurrImgIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
 
   useEffect(() => {
     getImages();
   }, []);
 
-  const onArrowClickHandler = (action) => {
-    setIsLoading(true);
-    let newIndex;
-
-    if (action === "next") {
-      newIndex = currImgIndex + 1 < imgUrls.length ? currImgIndex + 1 : 0;
-    } else {
-      newIndex = currImgIndex - 1 < 0 ? imgUrls.length - 1 : currImgIndex - 1;
-    }
-    setCurrImgIndex(newIndex);
-    setIsLoading(false);
-  };
-
   const getImages = async () => {
     setIsLoading(true);
     try {
       const urls = await fetchImages();
-      await setImgUrls(urls);
+      if (urls.length === 0) setIsEmpty(true);
+      setImgUrls(urls);
       setCurrImgIndex(0);
       setIsLoading(false);
     } catch (err) {
       console.log(`Faild to load images on: ${err}`);
     }
+  };
+
+  const onArrowClickHandler = (action) => {
+    setIsLoading(true);
+    let newIndex;
+
+    if (action === "next")
+      newIndex = currImgIndex + 1 < imgUrls.length ? currImgIndex + 1 : 0;
+    else
+      newIndex = currImgIndex - 1 < 0 ? imgUrls.length - 1 : currImgIndex - 1;
+
+    setCurrImgIndex(newIndex);
+    setIsLoading(false);
+  };
+
+  const showSlide = () => {
+    return !isEmpty ? (
+      <img src={imgUrls[currImgIndex]} alt="carusel-img" />
+    ) : (
+      <h2>No Images to show</h2>
+    );
   };
 
   return (
@@ -51,11 +61,7 @@ const ImageCarousel = () => {
         data-testid="prev-btn"
       />
       <SlideWrapper>
-        {isLoading ? (
-          <CircularProgress />
-        ) : (
-          <img src={imgUrls[currImgIndex]} alt="carusel-img" />
-        )}
+        {isLoading ? <CircularProgress /> : showSlide()}
       </SlideWrapper>
       <RightArrow
         onClick={() => {
--- src/task2/ecommerce.test.js
+++ src/task2/ecommerce.test.js
@@ -10,30 +10,25 @@ const ORDER_ID = "70ef599e5eca171b2bce84d1";
 
 test("Ecommerce: fetchOrderById", async () => {
   const orders = await fetchOrderById(ORDER_ID);
-  console.log(orders);
   expect(orders).toBeTruthy();
 });
 
 test("Ecommerce: fetchAllOrders", async () => {
   const allOrders = await fetchAllOrders();
-  console.log(allOrders);
   expect(allOrders).toBeTruthy();
 });
 
 test("Ecommerce: bucketOrdersByUsers", async () => {
   let orderByUsers = await bucketOrdersByUsers();
-  console.log(orderByUsers);
   expect(orderByUsers).toBeTruthy();
 });
 
 test("Ecommerce: getLast2WeeksOrders", async () => {
   let lastTwoWeeksOrders = await getLast2WeeksOrders();
-  console.log(lastTwoWeeksOrders);
   expect(lastTwoWeeksOrders).toBeTruthy();
 });
 
 test("Ecommerce: bucketOrdersByDate", async () => {
   let orderByDate = await bucketOrdersByDate();
-  console.log(orderByDate);
   expect(orderByDate).toBeTruthy();
 });
